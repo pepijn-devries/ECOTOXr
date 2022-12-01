13a09f1a4537885eda2942669c08dc8f8b2662aa
Vries <pepijn.devries@wur.nl>
2022-12-01 20:51:50 +0100

    Test that everything on TODO list is done

---
+++ tests/testthat/test_dev.r
@@ -0,0 +1,13 @@
+test_that("Source code does not have things on TODO list", {
+  expect_false({
+    files_to_check <- list.files(pattern = ".r$|NEWS|DESCRIPTION", recursive = T)
+    any(
+      unlist(
+        lapply(files_to_check, function(file) {
+          content <- suppressWarnings(readLines(file))
+          any(grepl("TODO", content))
+        })
+      )
+    )
+  })
+})
