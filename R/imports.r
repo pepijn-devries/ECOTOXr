0c805acd0ea3447fe979984bacd7bc713c4af435
pepijn-devries <pepijn-devries@users.noreply.github.com>
2025-02-05 09:29:46 +0100

    Update imports.r
    Removed .onAttach message for testing purposes

--- R/imports.r
+++ R/imports.r
@@ -1,13 +1,3 @@
-.onAttach <- function(libname, pkgname){
-  packageStartupMessage({
-    if (check_ecotox_availability()) {
-      crayon::green("ECOTOX database file located, you are ready to go!\n")
-    } else {
-      crayon::red("ECOTOX database file not present! Invoke download and\ndatabase build using 'download_ecotox_data()'\n")
-    }
-  })
-}
-
 #' @importFrom dplyr collect left_join inner_join select sql tbl
 #' @importFrom lifecycle badge
 #' @importFrom rlang := !!!
