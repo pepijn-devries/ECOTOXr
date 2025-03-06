81933d5d9838b77f89a2928f7c5a595c286d5140
pepijn-devries <peppi2@gmail.com>
2025-03-06 09:10:43 +0100

    Added more unit sanitation steps

--- R/process_unit.r
+++ R/process_unit.r
@@ -2,10 +2,10 @@
 #' 
 #' `r lifecycle::badge('experimental')` The function `search_ecotox()` returns fields
 #' from the ECOTOX database as is. Fields that represent units are not standardised in
-#'  the database. Therefore, this format is not consistently used throughout the
+#' the database. Therefore, this format is not consistently used throughout the
 #' database. `process_ecotox_units()` takes a `data.frame` returned by
 #' `search_ecotox()`, locates unit columns, represented by text, sanitises the text
-#' and converts them to `?units::units` objects. It will sanitise the unit fields as
+#' and converts them to `units::mixed_units()` objects. It will sanitise the unit fields as
 #' much as possible. *TODO* `NA`
 #' @param x A `data.frame` obtained with `search_ecotox()`, for which the units need
 #' to be processed.
@@ -112,24 +112,44 @@ as_unit_ecotox <- function(
       code          = trimws(.data$code),
       ## remove square brackets and commas
       code          = gsub("\\[|\\]|,", "", .data$code),
-      ## remove space after numeric followed by character
-      code          = gsub("(?<=[0-9]) (?=[a-z|A-Z])", "", .data$code, perl = TRUE),
-      ## remove AI and AE prefixes
-      code          = gsub("^(ai|AI|ae|AE) ", "", .data$code),
-      ## remove Carbon and organic matter annotation
-      code          = gsub(" [C|OM](?=$|[ :/*^])", "", .data$code, perl = TRUE),
+      ## remove known prefixes
+      code          = gsub("^(ae|AE|ai|AI|fl|litter) ", "", .data$code),
+      ## double spaces to single spaces
+      code          = gsub("  ", " ", .data$code),
+      ## remove spaces around multiply symbol
+      code          = gsub(" \\* ", "*", .data$code),
       ## standardise scientific notation in numeric component
       code          = gsub("10[xX]", "1e", .data$code),
+      ## interpret '0/00' as parts per thousand.
+      code          = gsub("0/00", "ppt", .data$code, fixed = TRUE),
       ## ensure 'wk' is parsed as week
       code          = gsub("wk", "week", .data$code, perl = TRUE),
       ## 'lbs' is not recognised as plural of 'lb' ## TODO not all lbs values are captured
       code          = gsub("(?<=^|[ :/*^])lbs(?=$|[ :/*^])", "lb", .data$code, perl = TRUE),
       ##TODO regex to match "Nm" between space, asterisk or forward slash, also at start or end
       code          = gsub("(?<=^|[ :/*^])Nm(?=$|[ :/*^])", "nm", .data$code, perl = TRUE),
-      code          = gsub("(?<=^|[ :/*^])BW(?=$|[ :/*^])", "bodyweight", .data$code, perl = TRUE),
-      ## TODO look for more unit annotations
-      code          = gsub("([ ]?)soil", "", .data$code, ignore.case = TRUE, perl = TRUE),
-      
+      ## 'ac' is 'acre'
+      code          = gsub("(?<=^|[ :/*^])ac(?=$|[ :/*^])", "acre", .data$code, perl = TRUE),
+      ## replace common count names with 'count'
+      code          = gsub("(?<=^|[ :/*^])(beat|(cell(s?))|no|org)(?=$|[ :/*^])", "count", .data$code, perl = TRUE),
+      ## remove various annotations (space followed by one of 'known' annotations)
+      code          = gsub(" (act|AE|arb|ash|bdwt|BO|bt|BW|C|C2H4|canopy|cap|CCT|CEC|change|clay|clitellate|CNTL|CO2|com|corn|cortex|CREA|dev|diet|DNA|dose|dry|DT|earliness|egg|eu|EU|FATL|fertile|fl|fluid|GAIN|H20|Hb|Hg|in|ingested|INHIB|initial|intake|lit|litter|max|media|min|MIT|N|NH3|node|O2|OC|of|oil|OM|org|P|PC|PLIPD|plt|pod|pro|prod|PRTL|ret|RI|RNA|S|sat|sd|sed|seed|soil|soln|solvent|SP|sperm|TI|TIME|tolerance|total|ureau|WDTH|wet|wght|WGHT|WSF|wt|yld|Zn)(?=$|[ :/*^])",
+                           "", .data$code, perl = TRUE),
+      ## percentage preceded by decade of grams is in the mass unit per decilitre
+      code          = gsub("g([ ]?)%( w/v)?", "g/dL", .data$code),
+      ## percentage concentration mass over volume is more explicitly
+      ## expressed as hectogram per millilitre
+      code          = gsub("^%([ ]?)(m|w)(:|/)v$", "hg/L", .data$code),
+      ## percentage concentration volume over volume is more explicitly
+      ## expressed as litre per hectolitre
+      code          = gsub("^%([ ]?)v(:|/)v$", "L/hL", .data$code),
+      ## percentage concentration mass over mass is more explicitly
+      ## expressed as grams per hectograms
+      code          = gsub("^%([ ]?)(m|w)(:|/)(m|w)$", "g/hg", .data$code),
+      ## percentage concentration volume over mass is more explicitly
+      ## expressed as grams per decalitre
+      code          = gsub("^%([ ]?)v(:|/)(m|w)$", "g/dL", .data$code),
+
       ## 'type' specific sanitation steps
       code          = if (type == "concentration") {
         
@@ -137,13 +157,15 @@ as_unit_ecotox <- function(
         ## in case of type 'concentration' 'K' stands for Karmen units
         result <- gsub("(?<=^|[ :/*^])K(?=$|[ :/*^])", "Karmen", result, perl = TRUE)
         ## in case of concentration dpm is disintegrations (counts) per minute
-        result <- gsub("(?<=^|[ :/*^])dpm(?=$|[ :/*^])", "counts/min", result, perl = TRUE)
+        result <- gsub("(?<=^|[ :/*^])(c|d)pm(?=$|[ :/*^])", "counts/min", result, perl = TRUE)
+        ## mM, uM, M is mmol/L, mmol/L and mol/L respectively
+        result <- gsub("((?<=^|[ :/*^])|(?<=^|[ :/*^][u|m]))M(?=$|[ :/*^])", "mol/L", result, perl = TRUE)
         result
         
       } else          if (type == "media") {
         
         result <- .data$code
-        ## in case of type 'media' 'C' stands for Celcius  
+        ## in case of type 'media' 'C' stands for Celsius  
         result <- gsub("(?<=^|[ :/*^])C(?=$|[ :/*^])", "Celsius", result, perl = TRUE)
         ## in case of type 'media' 'K' stands for Kelvin  
         result <- gsub("(?<=^|[ :/*^])K(?=$|[ :/*^])", "Kelvin", result, perl = TRUE)
@@ -151,22 +173,46 @@ as_unit_ecotox <- function(
         
       } else .data$code,
       
+      code          = {
+        ## scientific notation is not always parsed correctly by units package.
+        ## Therefore, the scientific notation is replaced by decimal notation
+        num <-
+          stringr::str_extract_all(.data$code, "\\d+[.]?\\d*e[-|+]?\\d+") |>
+          lapply(as.numeric) |>
+          lapply(\(y) {
+            if (length(y) == 0) return("")
+            format(y, scientific = abs(log10(y)) >= 9)
+          }) |>
+          unlist()
+        stringr::str_replace_all(.data$code, "\\d+[.]?\\d*e[-|+]?\\d+", num)
+      },
+      
+      ## remove space if preceded by numeric and followed by alphabetical character
+      code          = gsub("(?<=[0-9]) (?=[a-z|A-Z])", "", .data$code, perl = TRUE),
       ## rename all equivalents of 'day' to 'day'
-      code        = gsub("(?<=^|[ :/*^])BDAY|dayg|dayef|dayl|dayl|daym|dayv|hd|dapu|dbh|dph|dge|dla|dpe|dpel|dpes|dpf|dpfg|dpfl|dpgm|dph|dphv|dpm|dpmm|dpn|dpo|dpp|dpr|dpw|dpref|dps|dpu|dws|dpys(?=$|[ :/*^])", "day", .data$code, perl = TRUE),
+      code          = gsub("(?<=^|[ :/*^])BDAY|dapu|dayef|dayg|dayl|dayl|daym|dayv|dbh|dge|dla|dpe|dpel|dpes|dpf|dpfg|dpfl|dpgm|dph|dph|dphv|dpm|dpmm|dpn|dpo|dpp|dpr|dpref|dps|dpu|dpw|dpys|dws|hd(?=$|[ :/*^])", "day", .data$code, perl = TRUE),
       ## Use 'unit' by default as arbitrary unit
-      code        = ifelse(.data$code == "", "unit", .data$code)
+      code          = gsub("^(|--|NR|NC)$", "unit", .data$code)
     )
   
-  x <-
+  result <-
     x |>
-    pull("code") |>
-    lapply(
-      \(y) {
-        tryCatch({
-          units::mixed_units(1, y) |> fun()
-        }, error = function(e) units::mixed_units(1, "unit"))
-      }) |>
-    fun()
+    distinct() |>
+    mutate(
+      unit = {
+        y <-
+          lapply(
+            .data$code,
+            \(y) {
+              tryCatch({
+                units::mixed_units(1, y) |> fun()
+              }, error = function(e) units::mixed_units(1, "unit"))
+            }) |>
+          fun()
+        do.call(c, y)
+      }
+    )
   
-  do.call(c, x)
+  dplyr::left_join(x, result, "code") |>
+    pull("unit")
 }
